067576d7783fa001d88b49d7f604af94f7082715
umeshchau <umeshuc.94@gmail.com>
2022-06-24 00:15:57 +0530

    Added github work for heroku deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index () :
-    return "Starting Machine Learning Project"
+    return "Starting New Machine Learning Project"
 
 if __name__=="__main__":
      app.run(debug=True)
